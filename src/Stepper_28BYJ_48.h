173cad995e3cb07811afa81df37c90afa26475db
yangbo <gyangbo@gmail.com>
2020-10-23 20:50:23 +0800

    Update Stepper_28BYJ_48.h

--- src/Stepper_28BYJ_48.h
+++ src/Stepper_28BYJ_48.h
@@ -21,6 +21,7 @@ public:
 
 	Stepper_28BYJ_48(int pin_1n1, int pin_1n2, int pin_1n3, int pin_1n4);
     void step(int);
+    void stop();
    
 
 private:
@@ -29,6 +30,7 @@ private:
     int motorSpeed = 1200;  //variable to set stepper speed
 	int countsperrev = 512; // number of steps per full revolution
     int pin_1n1; int pin_1n2; int pin_1n3; int pin_1n4;
+    bool bStop;
 
 };
 
